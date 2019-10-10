b23d0d692c17ca8b4d16da23d6c8641851571949
GoranAlasov <alasov.jr@gmail.com>
2019-10-11 00:02:35 +0200

    listing all users after login

--- ElementarySchoolProject/frontend/src/LoginForm.js
+++ ElementarySchoolProject/frontend/src/LoginForm.js
@@ -1,61 +1,96 @@
 import React from 'react';
+import UsersList from './UsersList';
+
 
 class LoginForm extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             userName: '',
             password: '',
-            token: ''
+            token: '',
+            loginSuccess: false
         }
 
-        this.handleSubmit = this.handleSubmit.bind(this);
+        
     }    
 
-    handleSubmit(event) {
+    handleSubmit = async (event) => {
         event.preventDefault();
 
-        const getTokenEndpoint = 'http://localhost:57847/token';
-        const usr = event.target.username.value;
-        const pwd = event.target.password.value;
+        let getTokenEndpoint = 'http://localhost:57847/token';
 
-        fetch(getTokenEndpoint, {
+        let response = await fetch(getTokenEndpoint, {
             method: 'POST',
             header: {
                 'Content-Type': 'application/x-www-form-urlencoded'
             },
-            body: `grant_type=password&userName=${usr}&password=${pwd}`
-        })
-            .then(response => response.json())
-            .then(json => {
-                console.log('token is: ', json.access_token);
-                sessionStorage.setItem('token', json.access_token);
-                this.setState({
-                    userName: usr,
-                    password: pwd,
-                    token: `${json.access_token}`
-                }, function () {
-                    console.log(this.state.value);
-                })
-                
-            })            
-            .catch(error => {
-                console.log('error: ', error)
+            body: `grant_type=password&userName=${event.target.username.value}&password=${event.target.password.value}`
+        });
+
+        let jsonedResponse = await response.json();
+        let myToken = jsonedResponse.access_token;
+
+        if (myToken) {
+            //alert(myToken);
+            this.setState({
+                token: myToken,
+                loginSuccess: true
             })
+        }        
     }
 
+    // handleSubmit(event) {
+    //     event.preventDefault();
+
+    //     const getTokenEndpoint = 'http://localhost:57847/token';
+    //     const usr = event.target.username.value;
+    //     const pwd = event.target.password.value;
+
+    //     fetch(getTokenEndpoint, {
+    //         method: 'POST',
+    //         header: {
+    //             'Content-Type': 'application/x-www-form-urlencoded'
+    //         },
+    //         body: `grant_type=password&userName=${usr}&password=${pwd}`
+    //     })
+    //         .then(response => response.json())
+    //         .then(json => {
+    //             console.log('token is: ', json.access_token);
+    //             sessionStorage.setItem('token', json.access_token);
+    //             this.setState({
+    //                 userName: usr,
+    //                 password: pwd,
+    //                 token: `${json.access_token}`
+    //             }, function () {
+    //                 console.log(this.state.value);
+    //             })
+                
+    //         })            
+    //         .catch(error => {
+    //             console.log('error: ', error)
+    //         })
+    // }
+    
+
     render() {
-        return (
-            <div className="loginDiv">
-                <form onSubmit={this.handleSubmit}>
-                    <label>Username</label><br />
-                    <input type="text" name="username" ></input><br />
-                    <label>Password</label><br />
-                    <input type="password" name="password" ></input><br /><br />
-                    <input type="submit" value="Log In"></input>
-                </form>
-            </div>
-        );
+        if (this.state.loginSuccess) {
+            return (
+                <UsersList token={this.state.token}/>
+            );
+        } else {
+            return (
+                <div className="loginDiv">
+                    <form onSubmit={this.handleSubmit}>
+                        <label>Username</label><br />
+                        <input type="text" name="username" ></input><br />
+                        <label>Password</label><br />
+                        <input type="password" name="password" ></input><br /><br />
+                        <input type="submit" value="Log In"></input>
+                    </form>
+                </div>
+            );
+        }        
     }
 }
 
